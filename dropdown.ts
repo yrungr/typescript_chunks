cb1aa0a0f93a56c23b8c3fa871276b51d8dd0213
magnus michaelson <magnusmichaelson@protonmail.com>
2020-05-14 19:03:45 +0100

    :)

--- dropdown.ts
+++ dropdown.ts
@@ -5,7 +5,6 @@ optionElement.value = "value";
 selectElement.add(optionElement);
 selectElement.addEventListener('change',dropdown);
 function dropdown(){
-	console.log(selectElement.value);
 	var output: HTMLElement = <HTMLElement>document.getElementById('output');
 	output.innerText = selectElement.value;
 }
